2d17c90755de65ea080bff078773c91b66ffc60d
Jose Ponceleón <joseponceleon@gmail.com>
2024-10-14 19:16:11 -0500

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = ["*"]
 
 # FORM SUBMISSION
 # Comment out the following line and place your railway URL, and your production URL in the array.
-CSRF_TRUSTED_ORIGINS = ["*"]     
+CSRF_TRUSTED_ORIGINS = ["*", "faithful-delight-production.up.railway.app", "negocio.whasa.shop"]
  
 # Application definition
 
